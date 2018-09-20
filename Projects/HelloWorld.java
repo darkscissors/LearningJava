6b61bf908b7af04d0ea28b641ac2750fe141e568
darkscissors <darkscissors00@gmail.com>
2018-09-19 22:54:10 -0700

    First steps for billy

--- Projects/HelloWorld.java
+++ Projects/HelloWorld.java
@@ -1,5 +1,56 @@
 package Projects;
 
-public class HelloWorld {
+public class HelloWorld
+{
+  //hey isaac this line runs the code
+  public static void main(String args[])
+  {
+    //dfsiuohfdsiufiushfisudfdsh
+    /* hi
+    this is a long comment
 
+    its kinda cool
+    */
+
+    System.out.println("hi");
+    int coryAge = 20;
+    double isaacAge = 69.69;
+    String message = "this is a list of letters that java can use";
+    double totalAge  = coryAge + isaacAge; //
+    //34.5
+    System.out.println("Corys age is: " + coryAge + " isaacs age is: " + isaacAge);
+    //corys age is: 17 isaacs age is 17.5
+    System.out.println ("their combined ages are: " + totalAge);
+
+
+    /*
+    things to use in an if statement
+    > greater than
+    < less than
+    == equal to
+    !=  not equal to
+    >= greater or equal to
+    <= less or equal to
+    */
+    if (isaacAge >= 18)
+    {
+      //dsadd
+      System.out.println("you can sign up for club penguin");
+
+    }
+
+    int x = coryAge;
+
+
+
+    while(x > 0)
+    {
+
+      System.out.println(x);
+
+      x = x - 1;
+    }
+
+    // dsadsadsad
+  }
 }
